ed4523a4beae5de50a3d70d7529107d662d00c15
Nick Rudnick <rudnick@uni-trier.de>
2022-08-02 08:30:55 +0200

    220802 MonitoringInterface CoreToolbar

---
+++ src/components/core/CoreToolbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-toolbar app color="#9fcfff" dense>
+    <v-list>
+      <v-list-item v-for="k in subMenuKnowledge" :key="k.id">
+        <v-list-item-title>{{ k.id }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-toolbar>
+</template>
+
+<script lang="ts">
+import { subMenuKnowledge } from "@/assets/SubMenuKnowledge.json";
+export default {
+  data() {
+    return {
+      subMenuKnowledge: subMenuKnowledge,
+    };
+  },
+};
+</script>
---
+++ src/views/MonitoringInterface.vue
@@ -0,0 +1,23 @@
+<template>
+  <core-toolbar />
+  <hello-world />
+  <div file="MonitoringInterface"></div>
+  <hello-world />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+// Components
+import CoreToolbar from "@/components/core/CoreToolbar.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "MonitoringInterface",
+
+  components: {
+    CoreToolbar,
+    HelloWorld,
+  },
+});
+</script>
