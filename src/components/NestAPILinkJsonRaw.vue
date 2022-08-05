51335d37aceb3be6fc6f95d79d610550133e3fd6
Nick Rudnick <rudnick@uni-trier.de>
2022-08-05 13:16:11 +0200

    220805 NestAPILinkText cleanup

--- src/components/NestAPILinkJsonRaw.vue
+++ src/components/NestAPILinkJsonRaw.vue
@@ -17,7 +17,7 @@ export default defineComponent({
   },
   data() {
     return {
-      nestAPIFunctionResponse: null,
+      nestAPIFunctionResponse: "",
     };
   },
   methods: {
@@ -27,8 +27,9 @@ export default defineComponent({
   },
   created() {
     fetch(this.$props.nodeURL + "/api/" + this.$props.apiFunction)
-      .then((response) => response.json())
-      .then((data) => (this.nestAPIFunctionResponse = data));
+      .then((response) => response.text())
+//      .then((data) => (this.nestAPIFunctionResponse = JSON.stringify(JSON.parse('{"T_max":1152921504606846.8,"T_min":-1152921504606846.8,"adaptive_spike_buffers":true,"adaptive_target_buffers":true,"biological_time":0.0,"buffer_size_spike_data":2,"buffer_size_target_data":2,"connection_rules":["one_to_one","all_to_all","fixed_indegree","fixed_outdegree","pairwise_bernoulli","symmetric_pairwise_bernoulli","fixed_total_number"],"data_path":"","data_prefix":"","dict_miss_is_error":true,"growth_curves":["gaussian","linear","sigmoid"],"growth_factor_buffer_spike_data":1.5,"growth_factor_buffer_target_data":1.5,"keep_source_table":true,"local_num_threads":1,"local_spike_counter":0,"max_buffer_size_spike_data":8388608,"max_buffer_size_target_data":16777216,"max_delay":0.1,"max_num_syn_models":512}'))));
+      .then((data) => (this.nestAPIFunctionResponse = JSON.stringify(JSON.parse(data))));
   },
 });
 </script>
