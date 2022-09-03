49895c323377d8d4aa1c2025be127dd9a1e11fe6
Nick Rudnick <rudnick@uni-trier.de>
2022-09-04 01:41:02 +0200

    220904 load take store skeleton

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,26 @@
 import { defineStore } from "pinia";
 
+type SecsFromEpoch = number; // usage-friendly time stamp
+type Port = number; // here only localhost single process backends so far
+type CPUPerc = number; // CPU load in % of one CPU thread
+type MemKB = number; // memory usage in kB
+type Take = [SecsFromEpoch, Port];
+type Load = [CPUPerc, MemKB];
+
+export const useLoadTakerStore = defineStore("loadTaker", {
+  state: () => ({
+    loadTake: new Map<Take, Load>()
+  }),
+  getters: {
+//    double: (state) => state.loadTake * 2,
+  },
+  actions: {
+    takeLoad(take: Take, load: Load) {
+      this.loadTake.set(take, load);
+    },
+  },
+});
+
 export const useCounterStore = defineStore("counter", {
   state: () => ({
   		 count: 13
